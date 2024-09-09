3a2e079adea5682fd6c2ab334e8710cd03841660
fathurwalkers <fathurwalkers44@gmail.com>
2024-09-09 07:53:38 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\HomeController; 
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/testpage', [HomeController::class, 'testpage'])->name('testpage');
+});
